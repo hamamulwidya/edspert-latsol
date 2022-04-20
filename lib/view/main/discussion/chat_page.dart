ca795d9da4577f74224e9a271c8e206f898b97dd
Hamamul Fauzi <hamamul.fauzi@gmail.com>
2022-04-20 22:51:07 +0700

    halaman diskusi

--- lib/view/main/discussion/chat_page.dart
+++ lib/view/main/discussion/chat_page.dart
@@ -1,4 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:git_project/constants/r.dart';
+import 'package:intl/intl.dart';
 
 class ChatPage extends StatefulWidget {
   const ChatPage({Key? key}) : super(key: key);
@@ -8,12 +12,193 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  final textController = TextEditingController();
+  late CollectionReference chat;
+  late QuerySnapshot chatData;
+  // List<QueryDocumentSnapshot>? listChat;
+  // getDataFromFirebase() async {
+  //   chatData = await FirebaseFirestore.instance
+  //       .collection("room")
+  //       .doc("kimia")
+  //       .collection("chat")
+  //       .get();
+  //   // listChat = chatData.docs;
+  //   setState(() {});
+  //   // print(chatData.docs);
+  // }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    // getDataFromFirebase();
+  }
+
   @override
   Widget build(BuildContext context) {
+    chat = FirebaseFirestore.instance
+        .collection("room")
+        .doc("kimia")
+        .collection("chat");
+    final user = FirebaseAuth.instance.currentUser!;
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text("Diskusi Soal"),
+      ),
       body: Column(
-        children: [Text("CHAT")],
+        children: [
+          Expanded(
+            child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: StreamBuilder<QuerySnapshot>(
+                  stream: chat.orderBy("time").snapshots(),
+                  builder: (BuildContext context,
+                      AsyncSnapshot<QuerySnapshot<Object?>> snapshot) {
+                    if (!snapshot.hasData) {
+                      return CircularProgressIndicator();
+                    }
+
+                    return ListView.builder(
+                      itemCount: snapshot.data!.docs.reversed.length,
+                      reverse: true,
+                      itemBuilder: (BuildContext context, int index) {
+                        final currentChat = snapshot.data!.docs.reversed.toList()[index];
+                        final currentDate =
+                            (currentChat["time"] as Timestamp?)?.toDate();
+                        return Container(
+                          margin: EdgeInsets.only(bottom: 10),
+                          width: MediaQuery.of(context).size.width * 0.8,
+                          child: Column(
+                              crossAxisAlignment: user.uid == currentChat["uid"]
+                                  ? CrossAxisAlignment.end
+                                  : CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  currentChat["nama"],
+                                  style: TextStyle(
+                                    fontSize: 10,
+                                    color: Color(0xff5200FF),
+                                  ),
+                                ),
+                                Container(
+                                  padding: EdgeInsets.symmetric(
+                                      horizontal: 10, vertical: 4),
+                                  decoration: BoxDecoration(
+                                      color: user.uid == currentChat["uid"]
+                                          ? Colors.green.withOpacity(0.5)
+                                          : Color(0xffffdcdc),
+                                      borderRadius: BorderRadius.only(
+                                        bottomLeft: Radius.circular(10),
+                                        bottomRight:
+                                            user.uid == currentChat["uid"]
+                                                ? Radius.circular(0)
+                                                : Radius.circular(10),
+                                        topRight: Radius.circular(10),
+                                        topLeft: user.uid != currentChat["uid"]
+                                            ? Radius.circular(0)
+                                            : Radius.circular(10),
+                                      )),
+                                  child: Text(
+                                    currentChat["content"],
+                                  ),
+                                ),
+                                Text(
+                                  currentDate == null
+                                      ? ""
+                                      : DateFormat("dd-MMM-yyy HH:mm").format(currentDate),
+                                  style: TextStyle(
+                                    fontSize: 10,
+                                    color: R.colors.greySubtitleHome,
+                                  ),
+                                ),
+                              ]),
+                        );
+                      },
+                    );
+                  },
+                )),
+          ),
+          SafeArea(
+            child: Container(
+              decoration: BoxDecoration(color: Colors.white, boxShadow: [
+                BoxShadow(
+                    offset: Offset(0, -1),
+                    blurRadius: 10,
+                    color: Colors.black.withOpacity(0.25))
+              ]),
+              child: Row(
+                children: [
+                  IconButton(
+                    icon: Icon(
+                      Icons.add,
+                      color: Colors.blue,
+                    ),
+                    onPressed: () {},
+                  ),
+                  Expanded(
+                    child: Container(
+                      padding: EdgeInsets.symmetric(vertical: 4),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Container(
+                              height: 40,
+                              child: TextField(
+                                controller: textController,
+                                decoration: InputDecoration(
+                                  suffixIcon: IconButton(
+                                    icon: Icon(
+                                      Icons.camera_alt,
+                                      color: Colors.blue,
+                                    ),
+                                    onPressed: () {},
+                                  ),
+                                  contentPadding: EdgeInsets.zero,
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  hintText: "Tulis pesan disini..",
+                                  hintStyle: TextStyle(
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    icon: Icon(
+                      Icons.send,
+                      color: Colors.blue,
+                    ),
+                    onPressed: () {
+                      if (textController.text.isEmpty) {
+                        return;
+                      }
+                      print(textController.text);
+
+                      final chatContent = {
+                        "nama": user.displayName,
+                        "uid": user.uid,
+                        "content": textController.text,
+                        "email": user.email,
+                        "photo": user.photoURL,
+                        "file_url": "user.photoURL",
+                        "time": FieldValue.serverTimestamp(),
+                      };
+                      chat.add(chatContent).whenComplete(() {
+                        textController.clear();
+                      });
+                    },
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
