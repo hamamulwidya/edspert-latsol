89354116c8f1f28c8fcfc9fc34a515b025ca4406
Hamamul Fauzi <hamamul.fauzi@gmail.com>
2022-04-20 22:51:31 +0700

    endpoint api

---
+++ lib/constants/api_url.dart
@@ -0,0 +1,13 @@
+class ApiUrl {
+  static String baseUrl = "https://ekskul.id/api/";
+
+  static String users = "users";
+  static String userRegistrasi = "users/registrasi";
+  static String userUpdateProfile = "users/update";
+
+  static String latihanMapel = "exercise/data_course";
+  static String latihanPaketSoal = "";
+  static String latihanKerjakanSoal = "exercise/kerjakan";
+  static String latihanSubmitJawaban = "exercise/input_jawaban";
+  static String latihanSkor = "exercise/score_result";
+}
