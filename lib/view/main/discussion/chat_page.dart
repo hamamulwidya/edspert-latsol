bc23cf972fbba9e7ab7b1aac7a8f1a0a438002ca
Hamamul Fauzi <hamamul.fauzi@gmail.com>
2022-04-23 10:51:43 +0700

    firebas storage

--- lib/view/main/discussion/chat_page.dart
+++ lib/view/main/discussion/chat_page.dart
@@ -1,12 +1,16 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:git_project/constants/r.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:intl/intl.dart';
 
 class ChatPage extends StatefulWidget {
-  const ChatPage({Key? key}) : super(key: key);
-
+  const ChatPage({Key? key, this.id}) : super(key: key);
+  final String? id;
   @override
   State<ChatPage> createState() => _ChatPageState();
 }
@@ -62,7 +66,9 @@ class _ChatPageState extends State<ChatPage> {
                       itemCount: snapshot.data!.docs.reversed.length,
                       reverse: true,
                       itemBuilder: (BuildContext context, int index) {
-                        final currentChat = snapshot.data!.docs.reversed.toList()[index];
+                        final currentChat = snapshot.data!.docs.reversed
+                            .toList()[index] 
+                            ;
                         final currentDate =
                             (currentChat["time"] as Timestamp?)?.toDate();
                         return Container(
@@ -98,14 +104,26 @@ class _ChatPageState extends State<ChatPage> {
                                             ? Radius.circular(0)
                                             : Radius.circular(10),
                                       )),
-                                  child: Text(
-                                    currentChat["content"],
-                                  ),
+                                  child: currentChat["type"] == "file"
+                                      ? Image.network(
+                                          currentChat["file_url"],
+                                          errorBuilder:
+                                              (context, error, stackTrace) {
+                                            return Container(
+                                              padding: EdgeInsets.all(10),
+                                              child: Icon(Icons.warning),
+                                            );
+                                          },
+                                        )
+                                      : Text(
+                                          currentChat["content"],
+                                        ),
                                 ),
                                 Text(
                                   currentDate == null
                                       ? ""
-                                      : DateFormat("dd-MMM-yyy HH:mm").format(currentDate),
+                                      : DateFormat("dd-MMM-yyy HH:mm")
+                                          .format(currentDate),
                                   style: TextStyle(
                                     fontSize: 10,
                                     color: R.colors.greySubtitleHome,
@@ -151,7 +169,46 @@ class _ChatPageState extends State<ChatPage> {
                                       Icons.camera_alt,
                                       color: Colors.blue,
                                     ),
-                                    onPressed: () {},
+                                    onPressed: () async {
+                                      final imgResult = await ImagePicker()
+                                          .pickImage(
+                                              source: ImageSource.camera,
+                                              maxHeight: 500,
+                                              maxWidth: 500,
+                                              );
+
+                                      if (imgResult != null) {
+                                        File file = File(imgResult.path);
+                                        final name = imgResult.path.split("/");
+                                        String room = widget.id ?? "kimia";
+                                        String ref =
+                                            "chat/$room/${user.uid}/${imgResult.name}";
+
+                                        final imgResUpload =
+                                            await FirebaseStorage.instance
+                                                .ref()
+                                                .child(ref)
+                                                .putFile(file);
+
+                                        final url = await imgResUpload.ref
+                                            .getDownloadURL();
+
+                                        final chatContent = {
+                                          "nama": user.displayName,
+                                          "uid": user.uid,
+                                          "content": textController.text,
+                                          "email": user.email,
+                                          "photo": user.photoURL,
+                                          "ref": ref,
+                                          "type": "file",
+                                          "file_url": url,
+                                          "time": FieldValue.serverTimestamp(),
+                                        };
+                                        chat.add(chatContent).whenComplete(() {
+                                          textController.clear();
+                                        });
+                                      }
+                                    },
                                   ),
                                   contentPadding: EdgeInsets.zero,
                                   border: OutlineInputBorder(
@@ -186,7 +243,9 @@ class _ChatPageState extends State<ChatPage> {
                         "content": textController.text,
                         "email": user.email,
                         "photo": user.photoURL,
-                        "file_url": "user.photoURL",
+                        "ref": null,
+                        "type": "text",
+                        "file_url": null,
                         "time": FieldValue.serverTimestamp(),
                       };
                       chat.add(chatContent).whenComplete(() {
